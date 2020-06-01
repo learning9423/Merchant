d563d5737033645ad4ee84c642a8400092ce3966
jgli <jiangang.li@vova.com.hk>
2020-06-01 16:05:14 +0800

    添加了日志
    Signed-off-by: jgli <jiangang.li@vova.com.hk>

--- testsuite/test_suite.py
+++ testsuite/test_suite.py
@@ -1,23 +1,32 @@
+import logging
 import time
 import unittest
 from HTMLTestRunner import HTMLTestRunner
 
 
 class test_suite(unittest.TestCase):
+    spath='../testcases'
+    log_path='../log'
     def add_case(self):
         suite=unittest.TestSuite()
-        spath='../testcases'
-        suite.addTests(unittest.defaultTestLoader.discover(spath,pattern='test*.py'))
+
+        suite.addTests(unittest.defaultTestLoader.discover(self.spath,pattern='test*.py'))
         return suite
 
     def run_test(self):
 
         now=time.time()
         rpath=r'../report/商家后台接口测试'+str(now)+'report.html'
+        logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+                        filename=self.log_path + '/' + str(now) + r"result.log",
+                        filemode='w')
+        logger = logging.getLogger()
+        logger.debug(self.add_case())
         with open(rpath,'wb+') as f:
             runner=HTMLTestRunner(stream=f,title='商家后台测试报告',description='商家后台接口测试，本次测试接口为登录退出，上架下架',verbosity=2)
-            return runner.run(test_suite().add_case())
-
+            return runner.run(self.add_case())
 
 if __name__ == '__main__':
     test_suite().run_test()
