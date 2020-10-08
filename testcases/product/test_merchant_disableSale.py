9aebdc4c8c0bb02d1d6cace334b737ffa49c4b55
jgli <jiangang.li@vova.com.hk>
2020-10-08 13:58:19 +0800

    数据驱动
    Signed-off-by: jgli <jiangang.li@vova.com.hk>

--- testcases/product/test_merchant_disableSale.py
+++ testcases/product/test_merchant_disableSale.py
@@ -18,39 +18,10 @@ class DisableSale(unittest.TestCase):
                 elif self.disableSale_data[i]=='':
                     break
         except IndexError:
-
-
+                print('下标越界')#因为海外仓商品数量少，所以可能存在商品不足，赋值错误
         self.s = requests.session()
         self._type_equality_funcs={}
-
     def test_disableSale1(self):
-        '''无海外仓直接下架:token和商品id都正确'''
-        r=SendRequest.sendRequest(self.s,self.disableSale_data[0])
-        expect_result=self.disableSale_data[0]['expect_result'].split(":")[1]
-        msg=self.disableSale_data[0]['msg'].split(":")[1]
-
-        self.assertEqual(r.json()['execute_status'],eval(expect_result),msg=r.json())
-        self.assertEqual(r.json()['data']['code'],eval(msg),msg=r.json())
-
-    def test_disableSale2(self):
-        '''无海外仓直接下架:商品id错误'''
-        r=SendRequest.sendRequest(self.s,self.disableSale_data[1])
-        expect_result=self.disableSale_data[1]['expect_result'].split(":")[1]
-        msg=self.disableSale_data[1]['msg'].split(":")[1]
-
-        self.assertEqual(r.json()['execute_status'],eval(expect_result),msg=r.json())
-        self.assertEqual(r.json()['data']['code'],eval(msg),msg=r.json())
-
-    def test_disableSale3(self):
-        '''无海外仓库存直接下架:token错误'''
-        r=SendRequest.sendRequest(self.s,self.disableSale_data[2])
-        expect_result=self.disableSale_data[2]['expect_result'].split(":")[1]
-        msg=self.disableSale_data[2]['msg'].split(":")[1]
-
-        self.assertEqual(r.json()['execute_status'],eval(expect_result),msg=r.json())
-        self.assertEqual(r.json()['data']['code'],eval(msg),msg=r.json())
-
-    def test_disableSale4(self):
         '''有海外仓库存:直接下架'''
         r=SendRequest.sendRequest(self.s,self.disableSale_data[0])
         expect_result=self.disableSale_data[0]['expect_result'].split(":")[1]
@@ -59,14 +30,41 @@ class DisableSale(unittest.TestCase):
         self.assertEqual(r.json()['execute_status'],eval(expect_result),msg=r.json())
         self.assertEqual(r.json()['data']['code'],eval(msg),msg=r.json())
 
-    def test_disableSale5(self):
+    def test_disableSale2(self):
         '''有海外仓库存:清零标准仓库存不下架'''
-        r=SendRequest.sendRequest(self.s,self.disableSale_data[0])
-        expect_result=self.disableSale_data[0]['expect_result'].split(":")[1]
-        msg=self.disableSale_data[0]['msg'].split(":")[1]
+        r=SendRequest.sendRequest(self.s,self.disableSale_data[1])
+        expect_result=self.disableSale_data[1]['expect_result'].split(":")[1]
+        msg=self.disableSale_data[1]['msg'].split(":")[1]
+
+        self.assertEqual(r.json()['execute_status'],eval(expect_result),msg=r.json())
+        self.assertEqual(r.json()['data']['code'],eval(msg),msg=r.json())
+
+    def test_disableSale3(self):
+        '''无海外仓直接下架:token和商品id都正确'''
+        r=SendRequest.sendRequest(self.s,self.disableSale_data[2])
+        expect_result=self.disableSale_data[2]['expect_result'].split(":")[1]
+        msg=self.disableSale_data[2]['msg'].split(":")[1]
+
+        self.assertEqual(r.json()['execute_status'],eval(expect_result),msg=r.json())
+        self.assertEqual(r.json()['data']['code'],eval(msg),msg=r.json())
+
+    def test_disableSale4(self):
+        '''无海外仓直接下架:商品id错误'''
+        r=SendRequest.sendRequest(self.s,self.disableSale_data[3])
+        expect_result=self.disableSale_data[3]['expect_result'].split(":")[1]
+        msg=self.disableSale_data[3]['msg'].split(":")[1]
+
+        self.assertEqual(r.json()['execute_status'],eval(expect_result),msg=r.json())
+        self.assertEqual(r.json()['data']['code'],eval(msg),msg=r.json())
+
+    def test_disableSale5(self):
+        '''无海外仓库存直接下架:token错误'''
+        r=SendRequest.sendRequest(self.s,self.disableSale_data[4])
+        expect_result=self.disableSale_data[4]['expect_result'].split(":")[1]
+        msg=self.disableSale_data[4]['msg'].split(":")[1]
 
         self.assertEqual(r.json()['execute_status'],eval(expect_result),msg=r.json())
         self.assertEqual(r.json()['data']['code'],eval(msg),msg=r.json())
 
 if __name__ == '__main__':
-    DisableSale().test_disableSale1()
+    DisableSale().test_disableSale2()
