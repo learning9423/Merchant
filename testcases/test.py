1470b1ae11b687ee1aaa0d54d9a7aa00fea11198
jgli <jiangang.li@vova.com.hk>
2021-03-31 20:04:30 +0800

    更新商品信息完成
    Signed-off-by: jgli <jiangang.li@vova.com.hk>

--- testcases/test.py
+++ testcases/test.py
@@ -1,4 +1,24 @@
-import time
+from pip._vendor import requests
+
+
+class spider():
+
+    url_open='https://www.huya.com/g/lol'
+
+
+    def spider_lol(self):
+
+        r=requests.post(url=self.url_open)
+        html=r.content
+        return html
+
+    def
+
+
+    def go(self):
+        self.spider_lol()
+
 
 if __name__ == '__main__':
-    print(time.localtime())
+    r=spider()
+    r.go()
