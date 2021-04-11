01cede1a80754b1eab802138c716c7fde2a7fa05
home1 <15667021976@163.com>
2021-04-11 23:05:02 +0800

    name问题待解决

--- testcases/test.py
+++ testcases/test.py
@@ -6,7 +6,9 @@ from pip._vendor import requests
 
 class spider():
     url_lol = 'https://www.huya.com/g/lol'
-    root_pattern = r'<li class="game-live-item" gid="1" data-lp="5668003">([\s\S]*?)</li>' #正则根路径
+    root_pattern = r'<li class="game-live-item" gid="1" data-lp="[\d]*">([\s\S]*?)</li>' #正则根路径
+    name_pattern=r'<i class="nick" title="[\s\S]*">([\s\S]*?)</i>'
+    num_pattern=r'<i class="js-num">([\s\S]*?)</i>'
 
     def spider_lol(self):
         '''获取网页信息'''
@@ -16,9 +18,14 @@ class spider():
 
     def analysis(self, html):
         '''提取人名和人数'''
-        r = re.findall(self.root_pattern,html)
-        print(r)
-
+        anchor_html = re.findall(self.root_pattern,html)
+        anchors=[]
+        for anch in anchor_html:
+            name=re.findall(self.name_pattern,anch)
+            num=re.findall(self.num_pattern,anch)
+            anchor={"name":name,"num":num}
+            anchors.append(anchor)
+        print(anchors)
     def go(self):
         html = self.spider_lol()
         self.analysis(html)
